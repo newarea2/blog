363da2a9de54a43ec449968022bef9e5ba2f3c09
张宝晰 <zhangbaoxi913@sf-express.com>
2023-12-26 09:31:05 +0800

    feat: 开发环境和生产环境跨域解决

--- src/build-tool/vite/sidebar.ts
+++ src/build-tool/vite/sidebar.ts
@@ -3,7 +3,8 @@ import { SidebarArrayOptions } from "vuepress-theme-hope"
 const sidebar: SidebarArrayOptions = [
   { text: '配置', link: '/build-tool/vite/config' },
   { text: '依赖预构建', link: '/build-tool/vite/dependency-pre-bundling' },
-  { text: '部署', link: '/build-tool/vite/deploy' }
+  { text: '部署', link: '/build-tool/vite/deploy' },
+  { text: '如何解决跨域问题', link: '/build-tool/vite/cors' },
 ]
 
 export default sidebar
